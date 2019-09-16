77b063aa9d9786ea2c0cf2fd6171669c4a20fb46
Mark Procter <mprocter12@gmail.com>
2019-09-16 18:59:05 -0400

    Configure pins as GPIOs

---
+++ hw02/configurePins.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# LEDS
+config-pin p9_19 gpio
+config-pin p9_20 gpio
+config-pin p9_21 gpio
+config-pin p9_22 gpio
+# Buttons
+config-pin p9_15 gpio
+config-pin p9_16 gpio
+config-pin p9_17 gpio
+config-pin p9_18 gpio
