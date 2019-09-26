d850b74b2ceb4f25f637c8a2881ead09a4468422
Demo User <debian@beaglebone.localdomain>
2019-09-26 13:02:10 +0000

    Final HW03 Commit

--- hw03/EtchASketch.py
+++ hw03/EtchASketch.py
@@ -14,6 +14,7 @@ import smbus
 bus = smbus.SMBus(2)
 matrix = 0x70 # Use address
 
+
 # rotary encoders
 rotary_vert = RotaryEncoder(eQEP1)
 rotary_horiz = RotaryEncoder(eQEP2)
@@ -24,6 +25,8 @@ rotary_horiz.enable()
 
 # led matrix var
 display = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+displayEmpty = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+
 
 # Start oscillator (p10)
 bus.write_byte_data(matrix, 0x21, 0)
@@ -38,50 +41,46 @@ prev_horz_pos = rotary_horiz.position
 cursor = [3, 3]
 prevSpots = [[' '] * 8 for i in range(8)]
 prevSpots[cursor[0]][cursor[1]] = '*'
+try:
+    while True:
+    
+        vert_pos = rotary_vert.position
+        horz_pos = rotary_horiz.position
+        
+        if vert_pos > prev_vert_pos: # up
+            if cursor[1] < 7:
+                cursor[1] += 1
+            prev_vert_pos = vert_pos
+            prevSpots[cursor[0]][cursor[1]] = '*'
+        elif vert_pos < prev_vert_pos: # down
+            if cursor[1]  > 0:
+                cursor[1] -= 1
+            prev_vert_pos = vert_pos
+            prevSpots[cursor[0]][cursor[1]] = '*'
+        
+        # handle horizontal movement
+        if horz_pos > prev_horz_pos: #right
+            if cursor[0] > 0:
+                cursor[0] -= 1
+            prev_horz_pos = horz_pos
+            prevSpots[cursor[0]][cursor[1]] = '*'
+        elif horz_pos < prev_horz_pos: #left
+            if cursor[0] < 7:
+                cursor[0] += 1
+            prev_horiz_pos = horz_pos
+            prevSpots[cursor[0]][cursor[1]] = '*'
+            
+            
+        display[2*cursor[0]] = display[2*cursor[0]]|(0x80>>cursor[1])
+        bus.write_i2c_block_data(matrix, 0, display)
+            
+        time.sleep(0.1)
+    #end while
 
-while True:
-    
-    # for y in range(8):
-    #     for x in range(8):
-    #         if y == cursor[0] and x == cursor[1]:
-    #             display[2*x + 1] = display[2*x + 1] + pow(2, y)
-    #         if prevSpots[y][x] == '*':
-    #             display[2*x] = display[2*x] + pow(2, y)  
-	
-	
-    # bus.write_i2c_block_data(matrix, 0, display)
-    
-    vert_pos = rotary_vert.position
-    horz_pos = rotary_horiz.position
-    
-    if vert_pos > prev_vert_pos: # up
-        if cursor[1] < 7:
-            cursor[1] += 1
-        prev_vert_pos = vert_pos
-        prevSpots[cursor[0]][cursor[1]] = '*'
-    elif vert_pos < prev_vert_pos: # down
-        if cursor[1]  > 0:
-            cursor[1] -= 1
-        prev_vert_pos = vert_pos
-        prevSpots[cursor[0]][cursor[1]] = '*'
+except KeyboardInterrupt:
+    bus.write_i2c_block_data(matrix, 0, displayEmpty)
 
-    # handle horizontal movement
-    if horz_pos > prev_horz_pos: #right
-        if cursor[0] > 0:
-            cursor[0] -= 1
-        prev_horz_pos = horz_pos
-        prevSpots[cursor[0]][cursor[1]] = '*'
-    elif horz_pos < prev_horz_pos: #left
-        if cursor[0] < 7:
-            cursor[0] += 1
-        prev_horiz_pos = horz_pos
-        prevSpots[cursor[0]][cursor[1]] = '*'
-        
-        
-    display[2*cursor[0]] = display[2*cursor[0]]|(0x80>>cursor[1])
-    bus.write_i2c_block_data(matrix, 0, display)
-        
-    time.sleep(0.1)
-#end while
+
+
 
 
